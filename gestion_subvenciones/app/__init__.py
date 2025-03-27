cb62e25f6f4f2d6cd69272d78e37f6d43532d4ee
vcaruncho <cholinyo@gmail.com>
2025-03-27 18:12:18 +0100

    Reestructuración limpia

--- gestion_subvenciones/app/__init__.py
+++ gestion_subvenciones/app/__init__.py
@@ -1,1 +1,10 @@
-# Inicializador del paquete Flask
+﻿from flask import Flask
+from .models import db
+from .routes import main  # AsegÃºrate de que defines un blueprint 'main' en routes.py
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('config.Config')  # AsegÃºrate de tener un archivo config.py con la clase Config
+    db.init_app(app)
+    app.register_blueprint(main)
+    return app
